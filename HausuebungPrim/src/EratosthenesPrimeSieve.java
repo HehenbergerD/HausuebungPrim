ef183eef01e96a9dfdaf521b727681630a57e642
David Hehenberger <HehenbergerD22126@sus.htl-grieskirchen.at>
2024-09-17 15:27:52 +0200

    Code updated

--- HausuebungPrim/src/EratosthenesPrimeSieve.java
+++ HausuebungPrim/src/EratosthenesPrimeSieve.java
@@ -1,2 +1,27 @@
-package PACKAGE_NAME;public class EratosthenesPrimeSieve {
+public class EratosthenesPrimeSieve implements PrimeSieve{
+
+    int maxNumber;
+
+    public EratosthenesPrimeSieve(int maxNumber) {
+        this.maxNumber = maxNumber;
+    }
+
+    @Override
+    public boolean isPrime(int p) {
+        for (int i = 2; i < p; i++) {
+            if (p % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public void printPrimes() {
+        for (int i = 2; i < maxNumber; i++) {
+            if (isPrime(i)) {
+                System.out.println(i);
+            }
+        }
+    }
 }
--- HausuebungPrim/src/Main.java
+++ HausuebungPrim/src/Main.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Höchste Zahl eingeben:  ");
+        int number = scanner.nextInt();
+        EratosthenesPrimeSieve eratosthenesPrimeSieve = new EratosthenesPrimeSieve(number);
+        eratosthenesPrimeSieve.printPrimes();
     }
 }
